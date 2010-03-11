87ea84d414c92b89037dfcda0b98878c10134329
Tuomas Kareinen <tkareine@gmail.com>
2010-03-11 03:21:24 +0200

    Add project description

--- project.clj
+++ project.clj
@@ -1,2 +1,3 @@
 (defproject dearithmetic "0.0.1-SNAPSHOT"
+  :description "Naive classical arbitrary-precision decimal arithmetic implementations on positive integers"
   :dependencies [[org.clojure/clojure "1.1.0"]])
